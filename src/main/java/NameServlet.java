1c524d27dafedf90908eb698bc88e62888a1ede9
Nicholas.Hubacek <Nicholas.Hubacek@gmail.com>
2023-05-15 15:40:07 -0500

    Finshed

---
+++ src/main/java/NameServlet.java
@@ -0,0 +1,19 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "NameServlet", urlPatterns = "/name")
+public class NameServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("<h1>Hello, World!</h1>");
+
+    }
+}
