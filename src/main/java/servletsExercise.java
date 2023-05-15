caaa022478a0d9ded8fbed2d51cacf2894ef9d1b
Nicholas.Hubacek <Nicholas.Hubacek@gmail.com>
2023-05-15 16:08:49 -0500

    Finished serv-exerise

--- src/main/java/servletsExercise.java
+++ src/main/java/servletsExercise.java
@@ -1,18 +1,17 @@
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
-import javax.servlet.*;
-import javax.servlet.http.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-
-@WebServlet(name = "NameServlet", urlPatterns = "/name")
-public class NameServlet extends HttpServlet {
+@WebServlet(name = "NameServlet", urlPatterns = "/hello")
+public class servletsExercise extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
         PrintWriter out = resp.getWriter();
-        out.println("<h1>Hello Kotlin!</h1>");
-
+        out.println("<h1>Hello World!</h1>");
     }
 }
